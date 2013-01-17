933fe1df69a3b112450f87ebe63de20c7a31bcbb
crccheck <c@crccheck.com>
2013-01-17 16:01:57 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# tx_lobbying
+
+
+Very early alpha
+
+States:
+
+1. Scraper for Lobbyist Registrations
+  * data has to be manually downloaded, unfortunately.
+
+2. Scraper for Cover Sheets
+
+3. Derive Interest data from Lobbyist Registrations
+  * de-duplicate
+
+4. Scraper for other report types
---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import find_packages
+from distutils.core import setup
+
+import tx_lobbying as app
+
+setup(
+    name=app.__name__,
+    version=app.__version__,
+    # author
+    # author_email
+    # url
+    packages=find_packages('.', exclude=('example_project*',)),
+    include_package_data=True,  # automatically include things from MANIFEST
+    license='Apache License, Version 2.0',
+    description=app.__doc__.strip(),
+    long_description=open('README.md').read(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Framework :: Django",
+    ],
+)
