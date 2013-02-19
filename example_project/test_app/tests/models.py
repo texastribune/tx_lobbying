3fde7968bf4b4fa7d1a5024b93de08d65c940c50
crccheck <c@crccheck.com>
2013-02-19 03:48:56 +0000

    add test to make sure Lobbyist get_stats is accurate

--- example_project/test_app/tests/models.py
+++ example_project/test_app/tests/models.py
@@ -11,5 +11,21 @@ class LobbyistTests(TestCase):
         lobbyist.make_stats()
         self.assertEqual(lobbyist.stats.count(), 0)
 
-        print ExpenseCoversheetFactory.create()
+        ExpenseCoversheetFactory.create(lobbyist=lobbyist,
+            year=2000, spent_guess="100")
         lobbyist.make_stats()
+        self.assertEqual(lobbyist.stats.count(), 1)
+        self.assertEqual(lobbyist.stats.get(year=2000).spent, 100)
+
+        ExpenseCoversheetFactory.create(lobbyist=lobbyist,
+            year=2000, spent_guess="200")
+        lobbyist.make_stats()
+        self.assertEqual(lobbyist.stats.count(), 1)
+        self.assertEqual(lobbyist.stats.get(year=2000).spent, 300)
+
+        ExpenseCoversheetFactory.create(lobbyist=lobbyist,
+            year=2001, spent_guess="400")
+        lobbyist.make_stats()
+        self.assertEqual(lobbyist.stats.count(), 2)
+        self.assertEqual(lobbyist.stats.get(year=2000).spent, 300)
+        self.assertEqual(lobbyist.stats.get(year=2001).spent, 400)
