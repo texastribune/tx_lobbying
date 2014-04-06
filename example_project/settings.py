2c0e1c811e040ee48eebcdc3b11ac91f952a9c50
crccheck <c@crccheck.com>
2014-04-05 21:54:59 -0500

    move tests into tx_lobbying app

--- example_project/settings.py
+++ example_project/settings.py
@@ -108,17 +108,11 @@ INSTALLED_APPS = [
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # Uncomment the next line to enable the admin:
     'django.contrib.admin',
-    # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
     'django.contrib.humanize',
 
-    # Uncomment the next line to get runshell_plus, shell_plus, etc
     'django_extensions',
     'tx_lobbying',
-
-    'example_project.test_app',
 ]
 
 # A sample logging configuration. The only tangible logging
--- tx_lobbying/tests.py
+++
@@ -1,3 +0,0 @@
-# empty tests.py
-#
-# test are in another package: example_project.test_app.tests
