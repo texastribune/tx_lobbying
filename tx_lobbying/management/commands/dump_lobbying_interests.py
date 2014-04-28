4ba886fd52f6f6e49d9eb5414833f9bcc3b76a3c
crccheck <c@crccheck.com>
2014-04-27 23:43:13 -0500

    add a command to dump interests to csv

---
+++ tx_lobbying/management/commands/dump_lobbying_interests.py
@@ -0,0 +1,47 @@
+import csv
+import codecs
+import cStringIO
+from django.core.management.base import BaseCommand, CommandError
+
+
+# https://docs.python.org/2/library/csv.html
+class UnicodeWriter(object):
+    """
+    A CSV writer which will write rows to CSV file "f",
+    which is encoded in the given encoding.
+    """
+    def __init__(self, f, dialect=csv.excel, encoding="utf-8", **kwds):
+        # Redirect output to a queue
+        self.queue = cStringIO.StringIO()
+        self.writer = csv.writer(self.queue, dialect=dialect, **kwds)
+        self.stream = f
+        self.encoder = codecs.getincrementalencoder(encoding)()
+
+    def writerow(self, row):
+        self.writer.writerow([s.encode("utf-8") for s in row])
+        # Fetch UTF-8 output from the queue ...
+        data = self.queue.getvalue()
+        data = data.decode("utf-8")
+        # ... and reencode it into the target encoding
+        data = self.encoder.encode(data)
+        # write to the target stream
+        self.stream.write(data)
+        # empty queue
+        self.queue.truncate(0)
+
+    def writerows(self, rows):
+        for row in rows:
+            self.writerow(row)
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        from tx_lobbying.models import Interest
+        from sys import stdout
+        writer = UnicodeWriter(stdout)
+        writer.writerow(['name', 'state'])  # header
+        for interest in Interest.objects.all():
+            writer.writerow([interest.name, interest.state])
+
+
+
