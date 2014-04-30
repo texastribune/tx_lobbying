42d5a8c1a2788d4f30a1ff7533214bf8f8c08299
crccheck <c@crccheck.com>
2014-04-29 22:18:11 -0500

    delete any interest stats when importing canonical names

--- tx_lobbying/scrapers/canonical_interests.py
+++ tx_lobbying/scrapers/canonical_interests.py
@@ -19,17 +19,20 @@ def go(path):
         for row in reader:
             try:
                 interest = Interest.objects.get(name=row['name'])
-                if not row['canonical']:
-                    if interest.canonical:
-                        print "remove", interest
-                        interest.canonical = None
-                        interest.save()
+                if not row['canonical'] and interest.canonical:
+                    print "remove", interest
+                    interest.stats.all().delete()
+                    interest.canonical.stats.all().delete()
+                    interest.canonical = None
+                    interest.save()
                 else:
                     canonical = Interest.objects.get(name=row['canonical'])
                     if interest.canonical != canonical:
                         print "set", interest, canonical
                         interest.canonical = canonical
                         interest.save(update_fields=('canonical', ))
+                        interest.stats.all().delete()
+                        canonical.stats.all().delete()
             except (Interest.DoesNotExist, Interest.MultipleObjectsReturned):
                 print "skip", interest
                 # TODO
