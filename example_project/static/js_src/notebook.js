67b5da9e3d82f914d2df48f6ca4d0014cdaa6d4d
crccheck <c@crccheck.com>
2014-05-03 00:31:30 -0500

    add breadcrumb nav to top

--- example_project/static/js_src/notebook.js
+++ example_project/static/js_src/notebook.js
@@ -1,17 +1,21 @@
 (function (exports, $) {
   'use strict';
   var $paper = $('#notebook');
+  var $nav = $('#breadcrumbs');
 
   var retrieve = function (url) {
     var $page = $('<div class="page"/>').appendTo($paper);
+    // TODO don't use load, just do ajax and manually parse to get TITLE and
+    // get rid of #main hack
+    $page.load(url + ' #main', function () {
+      console.log('load', arguments);
+    });
+    // TODO use title instead of url
+    $nav.append('<span>' + url + '</span>');
     $page.data('notebook', {
       url: url,
       retrievedAt: Date.now()
     });
-    $page.load(url + ' #main', function () {
-      console.log('load', arguments);
-    });
-    // $page.load(url + ' body');
   };
 
   // exports
