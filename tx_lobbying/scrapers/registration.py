422f2db4019934bb2124557d74973ad654252e18
crccheck <c@crccheck.com>
2014-05-04 11:15:52 -0500

    refactor to split reg row from overall csv processing

--- tx_lobbying/scrapers/registration.py
+++ tx_lobbying/scrapers/registration.py
@@ -48,83 +48,87 @@ def update_or_create_interest(row):
     return interest, address, created
 
 
+def process_row(row):
+    report_date = convert_date_format_YMD(row['RPT_DATE'])
+    year = row['YEAR_APPL']
+
+    reg_address, created = Address.objects.get_or_create(
+        address1=row['ADDRESS1'],
+        address2=row['ADDRESS2'],
+        city=row['CITY'],
+        state=row['STATE'],
+        zipcode=row['ZIPCODE'],
+    )
+
+    # lobbyist
+    # very basic `Lobbyist` info here, most of it actually comes
+    # from the coversheets.
+    default_data = dict(
+        name=row['LOBBYNAME'],
+        sort_name=row['SORTNAME'],  # not LOB_SORT like in coversheets
+        updated_at=report_date,
+        address=reg_address,
+    )
+    lobbyist, created = Lobbyist.objects.update_or_create(
+        filer_id=row['FILER_ID'],
+        defaults=default_data)
+    if created:
+        logger.info("LOBBYIST: %s" % lobbyist)
+
+    if row['CONCERNAME']:
+        # interest/concern/client
+        interest, interest_address, created = update_or_create_interest(row)
+    else:
+        interest_address = None
+        interest = None
+
+    # registration report
+    default_data = dict(
+        raw=json.dumps(row),
+        report_date=report_date,
+        year=year,
+        address=reg_address,
+    )
+    report, created = RegistrationReport.objects.update_or_create(
+        lobbyist=lobbyist,
+        report_id=row['REPNO'],
+        defaults=default_data)
+    if created:
+        logger.info("REPORT: %s" % report)
+
+    if interest:
+        # lobbyist M2M to `Interest` through `Compensation`
+        annum, created = LobbyistAnnum.objects.update_or_create(
+            lobbyist=lobbyist,
+            year=year)
+        # compensation
+        default_data = dict(
+            amount_high=int(round(float(row['NHIGH'] or "0"))),  # I hate myself
+            amount_low=int(round(float(row['NLOW'] or "0"))),
+            compensation_type=row['TYPECOPM'],
+            address=interest_address,
+            raw=json.dumps(row),
+            updated_at=report_date,
+        )
+        if row['STARTDT']:
+            default_data['start_date'] = row['STARTDT']
+        if row['TERMDATE']:
+            default_data['end_date'] = row['TERMDATE']
+        # WISHLIST move this amount_guess logic into the model
+        default_data['amount_guess'] = (default_data['amount_high'] +
+            default_data['amount_low']) / 2
+        Compensation.objects.update_or_create(
+            annum=annum,
+            interest=interest,
+            defaults=default_data)
+
+
 def scrape(path, logger=logger):
     logger.info("Processing %s" % path)
     with open(path, 'rb') as f:
         reader = DictReader(f)
         for row in reader:
-            report_date = convert_date_format_YMD(row['RPT_DATE'])
-            year = row['YEAR_APPL']
-
-            reg_address, created = Address.objects.get_or_create(
-                address1=row['ADDRESS1'],
-                address2=row['ADDRESS2'],
-                city=row['CITY'],
-                state=row['STATE'],
-                zipcode=row['ZIPCODE'],
-            )
-
-            # lobbyist
-            # very basic `Lobbyist` info here, most of it actually comes
-            # from the coversheets.
-            default_data = dict(
-                name=row['LOBBYNAME'],
-                sort_name=row['SORTNAME'],  # not LOB_SORT like in coversheets
-                updated_at=report_date,
-                address=reg_address,
-            )
-            lobbyist, created = Lobbyist.objects.update_or_create(
-                filer_id=row['FILER_ID'],
-                defaults=default_data)
-            if created:
-                logger.info("LOBBYIST: %s" % lobbyist)
-
-            if row['CONCERNAME']:
-                # interest/concern/client
-                interest, interest_address, created = update_or_create_interest(row)
-            else:
-                interest_address = None
-                interest = None
-
-            # registration report
-            default_data = dict(
-                raw=json.dumps(row),
-                report_date=report_date,
-                year=year,
-                address=reg_address,
-            )
-            report, created = RegistrationReport.objects.update_or_create(
-                lobbyist=lobbyist,
-                report_id=row['REPNO'],
-                defaults=default_data)
-            if created:
-                logger.info("REPORT: %s" % report)
-
-            if interest:
-                # lobbyist M2M to `Interest` through `Compensation`
-                annum, created = LobbyistAnnum.objects.update_or_create(
-                    lobbyist=lobbyist,
-                    year=year)
-                # compensation
-                default_data = dict(
-                    amount_high=int(round(float(row['NHIGH'] or "0"))),  # I hate myself
-                    amount_low=int(round(float(row['NLOW'] or "0"))),
-                    compensation_type=row['TYPECOPM'],
-                    address=interest_address,
-                    raw=json.dumps(row),
-                    updated_at=report_date,
-                )
-                if row['STARTDT']:
-                    default_data['start_date'] = row['STARTDT']
-                if row['TERMDATE']:
-                    default_data['end_date'] = row['TERMDATE']
-                # WISHLIST move this amount_guess logic into the model
-                default_data['amount_guess'] = (default_data['amount_high'] +
-                    default_data['amount_low']) / 2
-                Compensation.objects.update_or_create(
-                    annum=annum,
-                    interest=interest,
-                    defaults=default_data)
+            process_row(row)
 
 
 if __name__ == "__main__":
