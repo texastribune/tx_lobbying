36f421c2f8c271a8af076d0796b9f1f4582882f6
crccheck <c@crccheck.com>
2014-05-26 14:09:28 -0500

    elaborate the lobbyist endpoint with more stuffs

--- tx_lobbying/api.py
+++ tx_lobbying/api.py
@@ -4,7 +4,36 @@ from restless.models import serialize
 from . import models
 
 
+address_data = dict(
+    include=(
+        ('name', lambda x: unicode(x)),
+    ),
+)
+
+
 class GetLobbyistData(Endpoint):
     def get(self, request, filer_id):
         obj = models.Lobbyist.objects.get(filer_id=filer_id)
-        return serialize(obj)
+        return serialize(
+            obj,
+            fields=[
+                'id',
+                'name',
+                ('address', address_data),
+            ],
+            include=(
+                ('years', dict(
+                    fields=(
+                        'year',
+                        ('clients', dict(
+                            include=(
+                                ('address', address_data),
+                            ),
+                            exclude=(
+                                'nomenklatura_id',
+                            ),
+                        )),
+                    )
+                )),
+            )
+        )
--- tx_lobbying/tests/test_api.py
+++ tx_lobbying/tests/test_api.py
@@ -5,6 +5,9 @@ from ..factories import LobbyistFactory
 
 
 class UrlsTest(TestCase):
+    """
+    Just make sure these endpoints are listening.
+    """
     def test_lobbyist(self):
         lobbyist = LobbyistFactory()
         url = reverse('tx_lobbying:api:lobbyist_detail',
