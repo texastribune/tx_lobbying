ccff1ebb99dbce08408f729ec2b0b09ce3e44b8f
crccheck <c@crccheck.com>
2015-02-14 20:52:45 -0600

    update the geocode command to be a little more useful

--- tx_lobbying/management/commands/lobbying_geocode.py
+++ tx_lobbying/management/commands/lobbying_geocode.py
@@ -1,19 +1,31 @@
-from django.core.management.base import BaseCommand, CommandError
+from optparse import make_option
+
+from django.core.management.base import BaseCommand
 
 
 class Command(BaseCommand):
+    help = 'Geocode some addresses, waits 10 seconds'
+    option_list = BaseCommand.option_list + (
+        make_option('--random', action='store_true', dest='random',
+            help='Pick addresses at random'),
+    )
+
     def handle(self, *args, **options):
         from tx_lobbying.utils import geocode_address
         from tx_lobbying.models import Address
         from time import sleep
 
         # pull in the same order so it's repeatable
-        # TODO add option to order by '?' so it fills in randomly
         queryset = (
             Address.objects
             .filter(coordinate__isnull=True)
             .exclude(zipcode='')
         )
+        self.stdout.write('# of addresses to geocode: {}'.format(queryset.count()))
+
+        if options['random']:
+            # not very efficient, but is terse
+            queryset = queryset.order_by('?')
 
         try:
             for address in queryset:
