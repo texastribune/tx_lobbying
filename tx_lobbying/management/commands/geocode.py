704d20bca13e4a92447b3abbdb76068247ac1a12
crccheck <c@crccheck.com>
2015-02-15 13:22:41 -0600

    give addresses the same alias formatting

--- tx_lobbying/management/commands/geocode.py
+++ tx_lobbying/management/commands/geocode.py
@@ -1,6 +1,8 @@
 from optparse import make_option
+import os
 
 from django.core.management.base import BaseCommand
+import requests
 
 
 class Command(BaseCommand):
@@ -10,10 +12,27 @@ class Command(BaseCommand):
             help='Pick addresses at random'),
     )
 
+    def get_remaining_credits(self):
+        # if you run out of credits, go to
+        # https://geoservices.tamu.edu/UserServices/Payments/
+        api_key = os.environ.get('TAMU_API_KEY')
+        assert api_key
+        url = ('https://geoservices.tamu.edu/UserServices/Payments/Balance/'
+            'AccountBalanceWebServiceHttp.aspx?'
+            'version=1.0&apikey={}&format=csv'.format(api_key))
+        response = requests.get(url)
+        assert response.ok
+        key, credits = response.text.split(',')
+        assert key == api_key
+        return int(credits)
+
     def handle(self, *args, **options):
         from tx_lobbying.utils import geocode_address
         from tx_lobbying.models import Address
         from time import sleep
+
+        credit = self.get_remaining_credits()
+        self.stdout.write('# of api credits remaining: {}'.format(credit))
 
         # pull in the same order so it's repeatable
         queryset = (
@@ -28,7 +47,7 @@ class Command(BaseCommand):
             queryset = queryset.order_by('?')
 
         try:
-            for address in queryset:
+            for address in queryset[:credit]:
                 self.stdout.write('*' * 40)
                 self.stdout.write(address.get_display_name(sep=', '))
                 self.stdout.write(unicode(geocode_address(address)))
