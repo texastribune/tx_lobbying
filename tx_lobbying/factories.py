6cb9991377393142a10fa80a1b63e2fdfdbc936f
crccheck <c@crccheck.com>
2015-05-17 00:11:00 -0500

    update factory syntax for new version of factory boy

--- tx_lobbying/factories.py
+++ tx_lobbying/factories.py
@@ -16,19 +16,22 @@ from .models import (
 
 
 class AddressFactory(factory.DjangoModelFactory):
-    FACTORY_FOR = models.Address
+    class Meta:
+        model = models.Address
     state = u'TX'
 
 
 class InterestFactory(factory.DjangoModelFactory):
-    FACTORY_FOR = Interest
+    class Meta:
+        model = Interest
 
     name = factory.Sequence(lambda n: 'Interest {0}'.format(n))
     address = factory.SubFactory(AddressFactory)
 
 
 class LobbyistFactory(factory.DjangoModelFactory):
-    FACTORY_FOR = Lobbyist
+    class Meta:
+        model = Lobbyist
     filer_id = factory.Sequence(lambda n: n)
     updated_at = factory.LazyAttribute(lambda a: datetime.date.today())
     first_name = factory.LazyAttribute(lambda a: names.get_first_name())
@@ -39,13 +42,15 @@ class LobbyistFactory(factory.DjangoModelFactory):
 
 
 class LobbyistAnnumFactory(factory.DjangoModelFactory):
-    FACTORY_FOR = LobbyistAnnum
+    class Meta:
+        model = LobbyistAnnum
     lobbyist = factory.SubFactory(LobbyistFactory)
     year = 2013
 
 
 class CompensationFactory(factory.DjangoModelFactory):
-    FACTORY_FOR = Compensation
+    class Meta:
+        model = Compensation
     amount_high = factory.LazyAttribute(lambda a: random.randint(10000, 100000))
     amount_low = factory.LazyAttribute(lambda a: random.randint(0, a.amount_high))
     amount_guess = factory.LazyAttribute(lambda a: (a.amount_high + a.amount_low) / 2)
@@ -55,7 +60,8 @@ class CompensationFactory(factory.DjangoModelFactory):
 
 
 class RegistrationReportFactory(factory.DjangoModelFactory):
-    FACTORY_FOR = RegistrationReport
+    class Meta:
+        model = RegistrationReport
     lobbyist = factory.SubFactory(LobbyistFactory)
     report_id = factory.Sequence(int)
     report_date = '1970-01-01'
@@ -65,7 +71,8 @@ class RegistrationReportFactory(factory.DjangoModelFactory):
 
 
 class CoversheetFactory(factory.DjangoModelFactory):
-    FACTORY_FOR = Coversheet
+    class Meta:
+        model = Coversheet
     lobbyist = factory.SubFactory(LobbyistFactory)
     report_date = '1970-01-01'
     report_id = factory.Sequence(lambda n: n)
