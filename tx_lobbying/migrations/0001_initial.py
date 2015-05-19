947a91debe6a6930cb30aa0e24ce98c5cb24b8b7
crccheck <c@crccheck.com>
2015-05-19 00:41:58 -0500

    squash migrations the old way because django can't migrate hstore

--- tx_lobbying/migrations/0001_initial.py
+++ tx_lobbying/migrations/0001_initial.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
+import django.contrib.postgres.fields.hstore
 import django.contrib.gis.db.models.fields
 
 
@@ -28,32 +29,31 @@ class Migration(migrations.Migration):
                 'ordering': ('address1',),
                 'verbose_name_plural': 'addresses',
             },
-            bases=(models.Model,),
         ),
         migrations.CreateModel(
             name='Compensation',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('raw', models.TextField()),
+                ('raw', django.contrib.postgres.fields.hstore.HStoreField(null=True)),
                 ('amount_high', models.IntegerField()),
                 ('amount_low', models.IntegerField()),
                 ('compensation_type', models.CharField(max_length=20, null=True, blank=True)),
                 ('start_date', models.DateField(null=True, blank=True)),
                 ('end_date', models.DateField(null=True, blank=True)),
                 ('updated_at', models.DateField()),
+                ('client_num', models.PositiveSmallIntegerField(help_text=b'Client ID for lobbyist. The client number assigned to this client on the most recently filed registration.', null=True, blank=True)),
                 ('amount_guess', models.IntegerField()),
                 ('address', models.ForeignKey(blank=True, to='tx_lobbying.Address', help_text=b'The address the lobbyist listed for the `Interest`', null=True)),
             ],
             options={
                 'ordering': ('interest__name', 'annum__year', 'annum__lobbyist'),
             },
-            bases=(models.Model,),
         ),
         migrations.CreateModel(
             name='Coversheet',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('raw', models.TextField()),
+                ('raw', django.contrib.postgres.fields.hstore.HStoreField(null=True)),
                 ('report_date', models.DateField()),
                 ('report_id', models.PositiveIntegerField(unique=True)),
                 ('correction', models.PositiveSmallIntegerField(default=0, help_text=b'Correction Number (0=Original)')),
@@ -81,13 +81,12 @@ class Migration(migrations.Migration):
                 'ordering': ('report_date',),
                 'get_latest_by': 'year',
             },
-            bases=(models.Model,),
         ),
         migrations.CreateModel(
             name='ExpenseDetailReport',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('raw', models.TextField()),
+                ('raw', django.contrib.postgres.fields.hstore.HStoreField(null=True)),
                 ('idno', models.PositiveIntegerField()),
                 ('year', models.IntegerField()),
                 ('amount', models.DecimalField(default=0, null=True, max_digits=12, decimal_places=2)),
@@ -98,13 +97,13 @@ class Migration(migrations.Migration):
             options={
                 'ordering': ('cover__report_date',),
             },
-            bases=(models.Model,),
         ),
         migrations.CreateModel(
             name='Interest',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('name', models.CharField(unique=True, max_length=200)),
+                ('slug', models.SlugField(max_length=200, null=True, blank=True)),
                 ('nomenklatura_id', models.PositiveIntegerField(null=True, blank=True)),
                 ('address', models.ForeignKey(to='tx_lobbying.Address')),
                 ('canonical', models.ForeignKey(related_name='aliases', blank=True, to='tx_lobbying.Interest', null=True)),
@@ -112,7 +111,6 @@ class Migration(migrations.Migration):
             options={
                 'ordering': ('name',),
             },
-            bases=(models.Model,),
         ),
         migrations.CreateModel(
             name='InterestStats',
@@ -128,7 +126,6 @@ class Migration(migrations.Migration):
             options={
                 'ordering': ('year',),
             },
-            bases=(models.Model,),
         ),
         migrations.CreateModel(
             name='Lobbyist',
@@ -143,12 +140,12 @@ class Migration(migrations.Migration):
                 ('title', models.CharField(max_length=15)),
                 ('suffix', models.CharField(max_length=5)),
                 ('nick_name', models.CharField(max_length=25)),
+                ('slug', models.SlugField(max_length=100, null=True, blank=True)),
                 ('address', models.ForeignKey(blank=True, to='tx_lobbying.Address', null=True)),
             ],
             options={
                 'ordering': ('sort_name',),
             },
-            bases=(models.Model,),
         ),
         migrations.CreateModel(
             name='LobbyistAnnum',
@@ -161,7 +158,6 @@ class Migration(migrations.Migration):
             options={
                 'ordering': ('-year',),
             },
-            bases=(models.Model,),
         ),
         migrations.CreateModel(
             name='LobbyistStats',
@@ -191,23 +187,21 @@ class Migration(migrations.Migration):
             options={
                 'ordering': ('year',),
             },
-            bases=(models.Model,),
         ),
         migrations.CreateModel(
             name='RegistrationReport',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('raw', models.TextField()),
+                ('raw', django.contrib.postgres.fields.hstore.HStoreField(null=True)),
                 ('report_id', models.IntegerField(unique=True)),
                 ('report_date', models.DateField()),
                 ('year', models.IntegerField()),
-                ('address', models.ForeignKey(to='tx_lobbying.Address')),
+                ('address', models.ForeignKey(help_text=b'The address the lobbyist listed for themself.', to='tx_lobbying.Address')),
                 ('lobbyist', models.ForeignKey(related_name='registrations', to='tx_lobbying.Lobbyist')),
             ],
             options={
-                'ordering': ('year',),
+                'ordering': ('year', 'report_date'),
             },
-            bases=(models.Model,),
         ),
         migrations.CreateModel(
             name='Subject',
@@ -222,7 +216,36 @@ class Migration(migrations.Migration):
             options={
                 'ordering': ('category_id', 'slug'),
             },
-            bases=(models.Model,),
+        ),
+        migrations.AddField(
+            model_name='expensedetailreport',
+            name='lobbyist',
+            field=models.ForeignKey(related_name='expensedetails', to='tx_lobbying.Lobbyist'),
+        ),
+        migrations.AddField(
+            model_name='coversheet',
+            name='lobbyist',
+            field=models.ForeignKey(related_name='coversheets', to='tx_lobbying.Lobbyist'),
+        ),
+        migrations.AddField(
+            model_name='coversheet',
+            name='subjects',
+            field=models.ManyToManyField(related_name='reports', to='tx_lobbying.Subject'),
+        ),
+        migrations.AddField(
+            model_name='compensation',
+            name='annum',
+            field=models.ForeignKey(to='tx_lobbying.LobbyistAnnum'),
+        ),
+        migrations.AddField(
+            model_name='compensation',
+            name='interest',
+            field=models.ForeignKey(to='tx_lobbying.Interest'),
+        ),
+        migrations.AddField(
+            model_name='compensation',
+            name='report',
+            field=models.ForeignKey(blank=True, to='tx_lobbying.RegistrationReport', null=True),
         ),
         migrations.AlterUniqueTogether(
             name='lobbyiststats',
@@ -236,42 +259,16 @@ class Migration(migrations.Migration):
             name='intereststats',
             unique_together=set([('interest', 'year')]),
         ),
-        migrations.AddField(
-            model_name='expensedetailreport',
-            name='lobbyist',
-            field=models.ForeignKey(related_name='expensedetails', to='tx_lobbying.Lobbyist'),
-            preserve_default=True,
-        ),
         migrations.AlterUniqueTogether(
             name='expensedetailreport',
             unique_together=set([('idno', 'type')]),
         ),
-        migrations.AddField(
-            model_name='coversheet',
-            name='lobbyist',
-            field=models.ForeignKey(related_name='coversheets', to='tx_lobbying.Lobbyist'),
-            preserve_default=True,
-        ),
-        migrations.AddField(
-            model_name='coversheet',
-            name='subjects',
-            field=models.ManyToManyField(related_name='reports', to='tx_lobbying.Subject'),
-            preserve_default=True,
-        ),
-        migrations.AddField(
-            model_name='compensation',
-            name='annum',
-            field=models.ForeignKey(to='tx_lobbying.LobbyistAnnum'),
-            preserve_default=True,
-        ),
-        migrations.AddField(
-            model_name='compensation',
-            name='interest',
-            field=models.ForeignKey(to='tx_lobbying.Interest'),
-            preserve_default=True,
-        ),
         migrations.AlterUniqueTogether(
             name='compensation',
             unique_together=set([('annum', 'interest')]),
+        ),
+        migrations.AlterUniqueTogether(
+            name='address',
+            unique_together=set([('address1', 'city', 'state', 'zipcode')]),
         ),
     ]
