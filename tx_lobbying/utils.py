a8f15e99327fb7aedeed9c046c70b45b00587a44
crccheck <c@crccheck.com>
2015-05-22 19:36:27 -0500

    change stats utils to use a progressbar

--- tx_lobbying/utils.py
+++ tx_lobbying/utils.py
@@ -3,6 +3,7 @@ import os
 
 from django.core.exceptions import ImproperlyConfigured
 from django.db.models import Count
+from tqdm import tqdm
 import requests
 
 from .models import Address, Interest, Lobbyist
@@ -14,18 +15,15 @@ logger = logging.getLogger(__name__)
 def update_lobbyists_stats(starting=None):
     """Update `Lobbyist` expense stats."""
     qs = Lobbyist.objects.all()
-    # qs = qs.filter(updated_at__gte=starting)
-    count = qs.count()
-    for i, l in enumerate(qs, 1):
-        logger.info("{:>4} / {} - {}".format(i, count, l))
+    total = qs.count()
+    for l in tqdm(qs, total=total, leave=True):
         l.make_stats()
 
 
 def update_interests_stats():
     qs = Interest.objects.filter(canonical__isnull=True)
-    count = qs.count()
-    for i, interest in enumerate(qs, 1):
-        logger.info(u'{:>4} / {} - {}'.format(i, count, interest))
+    total = qs.count()
+    for interest in tqdm(qs, total=total, leave=True):
         interest.make_stats()
 
 
